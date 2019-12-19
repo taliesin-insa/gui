83c9d2ce514a6485e4c7851bb88a868d589b9f21
mdarfay <mdarfay@insa-rennes.fr>
2019-12-19 10:40:13 +0100

    feat(HomePage): Creation of the component

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
