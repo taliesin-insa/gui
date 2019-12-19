fd4cf189fd2ffc25f9f06b9ce67254ca806a781b
mdarfay <mdarfay@insa-rennes.fr>
2019-12-19 10:43:19 +0100

    feat(DBManagement): Creation of the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AnnotationComponent } from './annotation/annotation.component';
+import { DbManagementComponent } from './db-management/db-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    AnnotationComponent
+    AnnotationComponent,
+    DbManagementComponent
   ],
   imports: [
     BrowserModule,
