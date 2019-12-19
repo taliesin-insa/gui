20b8a2ad1f5fc61cd0f267e3b3fb30c9cc3e28ec
Matthieu Darfay <matthieu.darfay@insa-rennes.fr>
2019-12-19 15:11:20 +0000

    feat(General): Routing inside the different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AnnotationComponent} from './annotation/annotation.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {DbManagementComponent} from './db-management/db-management.component';
+import {DbCreationComponent} from './db-creation/db-creation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'annotation', component: AnnotationComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'dbManagement', component: DbManagementComponent },
+  { path: 'dbCreation', component: DbCreationComponent },
+  { path: '',    redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
