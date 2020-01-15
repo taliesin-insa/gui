73e6f2f6386f392345e78c843c6c760ae9cac0eb
mdarfay <mdarfay@insa-rennes.fr>
2020-01-15 22:19:33 +0100

    fix(Annotation): Add ID attribute in class Snippet to match data sent by micro-database

--- src/app/model/Snippet.ts
+++ src/app/model/Snippet.ts
@@ -1,8 +1,10 @@
 export class Snippet {
+  id: number;
   url: string;
   value: string;
 
-  constructor(url: string, value: string) {
+  constructor(id: number, url: string, value: string) {
+    this.id = id;
     this.url = url;
     this.value = value;
   }
