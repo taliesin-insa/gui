ffa527587a0ce5d9bcaa42d90c6c9a15337d5350
mdarfay <mdarfay@insa-rennes.fr>
2020-01-19 14:27:55 +0100

    refactor(General): Update messages sent to error message service by HTTP error handler

--- src/app/services/http-error-handler.service.ts
+++ src/app/services/http-error-handler.service.ts
@@ -34,15 +34,18 @@ export class HttpErrorHandler {
       let message;
       switch (error.status) {
         case 404:
-          message = `Server or URL not found`;
+          message = `Server or URL not found  (${error.status})`;
           break;
         case 504:
-          message = `An internal server error occured, please try again`;
+          message = `An internal server error occured, please try again  (${error.status})`;
           break;
         default:
-          message = `server returned code ${error.status} with body "${error.error}"`;
+          if ( error.error instanceof ErrorEvent ) {
+            message = error.error.message;
+          } else {
+            message = `server returned code ${error.status} with body "${error.error}"`;
+          }
       }
-      message += ` (${error.status})`;
 
       // TODO: better job of transforming error for user consumption
       this.errorMessageService.add(`${serviceName}: ${operation} failed: ${message}`);
