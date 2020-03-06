618363f348bf2910305bf275c60991d93ce186f4
Zoé Levrel <zoe.levrel@insa-rennes.fr>
2020-03-06 14:15:23 +0100

    fix(DBManagement): previous commit doesn't work

--- src/app/db-management/db-management.component.ts
+++ src/app/db-management/db-management.component.ts
@@ -27,7 +27,7 @@ export class DbManagementComponent implements OnInit {
    * Gets all the piff files and their image and shows a "save as" dialog to the user
    */
   exportPiFF() {
-    this.http.get('/export/piff')
+    this.http.get('/export/piff', { responseType : 'blob'})
       .pipe(
         map(response => (response) as Blob),
         catchError(this.handleError('exportPiFF', null)))
