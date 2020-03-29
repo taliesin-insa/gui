2fa66eef28237b2ee5dd4da1c7b57129ad194a00
Florian Le Minoux <florian.le-minoux@insa-rennes.fr>
2020-03-29 18:56:12 +0200

    feat(Login): adding initial support for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {Router} from '@angular/router';
 import {getIdAndValue, getUnreadableFlag, Snippet} from '../model/Snippet';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Form} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
 import {HandleError, HttpErrorHandler} from '../services/http-error-handler.service';
+import {SessionStorageService} from '../services/session-storage.service';
+import {AuthService} from '../services/auth.service';
 import {catchError} from 'rxjs/operators';
 import {ToastService} from '../toast-global/toast-service';
 
@@ -15,8 +17,31 @@ import {ToastService} from '../toast-global/toast-service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  loginForm;
+
+  constructor(private router: Router, private session: SessionStorageService, private formBuilder: FormBuilder, private auth: AuthService) {
+    this.loginForm = this.formBuilder.group({
+      username: '',
+      password: '',
+    });
+  }
 
   ngOnInit() {
+    if (this.session.getToken()) {
+      // user is already logged in
+      // TODO: redirect to home page
+    }
+  }
+
+  onSubmit() {
+    console.log('submitted');
+
+    this.auth.login(this.loginForm).subscribe(data => {
+      console.log(data);
+      this.session.saveToken(data.token);
+      this.session.saveUser(data);
+    }, err => {
+      console.error(err);
+    });
   }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials): Observable<any> {
+    return this.http.post('/auth/login', {
+      username: credentials.value.username,
+      password: credentials.value.password
+    }, httpOptions);
+  }
+
+}
