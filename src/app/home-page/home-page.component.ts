eb75a4e532fe38be9fd2c9977f0954b5684f07c1
Florian Le Minoux <florian.le-minoux@insa-rennes.fr>
2020-03-29 17:47:11 +0200

    refactor(HomeComponent): changing error handling method

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
-import {ToastService} from '../toast-global/toast-service';
+import {ErrorMessageService} from '../services/error-messages.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -12,14 +12,15 @@ export class HomePageComponent implements OnInit {
 
   private statusData: any;
 
-  constructor(private router: Router, private route: ActivatedRoute, private toastService: ToastService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private errorMessageService: ErrorMessageService) {
+  }
 
   ngOnInit() {
     this.statusData = this.route.snapshot.data.statusData;
     if (this.statusData !== null && this.statusData.isDBUp && this.statusData.total === 0)  {
       this.router.navigate(['/dbCreation']);
     } else if (this.statusData !== null && !this.statusData.isDBUp) {
-      this.toastService.showDanger('DB is not up');
+      this.errorMessageService.add(`Snippets database is not available`);
     }
   }
 
