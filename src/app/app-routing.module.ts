ca4f040111a9a60036401ef118c2e77a7deb3546
Florian Le Minoux <florian.le-minoux@insa-rennes.fr>
2020-03-29 19:38:53 +0200

    feat(Login): making login mandatory to use the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,20 @@ import {DbManagementComponent} from './db-management/db-management.component';
 import {DbCreationComponent} from './db-creation/db-creation.component';
 import {StatusResolverService} from './services/data-resolver.service';
 import {LoginComponent} from './login/login.component';
-
+import {AuthGuard} from './auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'annotation', component: AnnotationComponent },
-  { path: 'home', component: HomePageComponent },
+  { path: 'annotation', component: AnnotationComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
   { path: 'dbManagement',
     component: DbManagementComponent,
     resolve: { // describes what to do before loading the component
-    statusData: StatusResolverService // name of the returned variable: name of the class which provides the service
-    }
+      statusData: StatusResolverService // name of the returned variable: name of the class which provides the service
+    },
+    canActivate: [AuthGuard]
   },
-  { path: 'dbCreation', component: DbCreationComponent },
+  { path: 'dbCreation', component: DbCreationComponent, canActivate: [AuthGuard] },
   { path: '',    redirectTo: '/home', pathMatch: 'full'}
 ];
 
