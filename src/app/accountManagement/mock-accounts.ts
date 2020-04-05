2a496b3b24a07031cf2e93248cf514cf8a24be51
Nathan Balix <nathan.balix@insa-rennes.fr>
2020-04-05 15:51:40 +0200

    Style(AccManagement):Mock-up de la page avec fonction de suppression

---
+++ src/app/accountManagement/mock-accounts.ts
@@ -0,0 +1,8 @@
+import {Account} from './account';
+
+export const ACCOUNTS: Account[] = [
+  {email: 'riri@gmail.com', name: 'riri', password: 'Mot', role: false},
+  {email: 'fifi@gmail.com', name: 'fifi', password: 'de', role: false},
+  {email: 'loulou@gmail.com', name: 'loulou', password: 'passe', role: false},
+  {email: 'picsou@gmail.com', name: 'picsou', password: 'admin', role: true},
+];
