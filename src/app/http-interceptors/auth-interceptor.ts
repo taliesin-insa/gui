19ea75ab132e84a63d9d2fed6cbe027c32ad10c6
darkgallium <florian+github@aloneinthedark.xyz>
2020-04-07 11:39:10 +0200

    refactor(Login): simplify Authorization header

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -14,7 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
     let authReq = req;
     const token = this.session.getToken();
     if (token != null) {
-      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
     }
     return next.handle(authReq);
   }
