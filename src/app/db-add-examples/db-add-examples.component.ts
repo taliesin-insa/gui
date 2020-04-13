905941b8e96770d6ef356253dd5feb57265c84cc
Zoé Levrel <zoe.levrel@insa-rennes.fr>
2020-04-13 13:54:11 +0200

    feat(AddExamples): create files

---
+++ src/app/db-add-examples/db-add-examples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-db-add-examples',
+  templateUrl: './db-add-examples.component.html',
+  styleUrls: ['./db-add-examples.component.scss']
+})
+export class DbAddExamplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
