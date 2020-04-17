70cf6aa87dfcb5b5703c2414fd81a2b5946c96eb
Zoé Levrel <zoe.levrel@insa-rennes.fr>
2020-04-17 13:57:41 +0200

    docs(README): add AddExamples scope

--- src/app/db-management/db-management.component.ts
+++ src/app/db-management/db-management.component.ts
@@ -74,6 +74,7 @@ export class DbManagementComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    console.log('PASSAGE DANS NG ON DESTROY');
     this.httpErrorHandler.clearErrors();
   }
 
