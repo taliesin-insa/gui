efed0bff9bb6f677c2436cb10dc441ecb3dffd54
darkgallium <florian+github@aloneinthedark.xyz>
2020-04-19 16:11:20 +0200

    refactor(General): prevent redirect loop if user is not admin and db is not created

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
 import {ErrorMessageService} from '../services/error-messages.service';
 import {AppComponent} from '../app.component';
+import { SessionStorageService } from '../services/session-storage.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -14,12 +15,15 @@ export class HomePageComponent implements OnInit {
   private statusData: any;
 
   // tslint:disable-next-line:max-line-length
-  constructor(private router: Router, private route: ActivatedRoute, private errorMessageService: ErrorMessageService, private appComponent: AppComponent) {
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private errorMessageService: ErrorMessageService,
+              private session: SessionStorageService) {
   }
 
   ngOnInit() {
     this.statusData = this.route.snapshot.data.statusData;
-    if (this.statusData !== null && this.statusData.isDBUp && this.statusData.total === 0)  {
+    if (this.statusData !== null && this.statusData.isDBUp && this.statusData.total === 0 && this.session.getUser()[`Role`] === 0)  {
       this.router.navigate(['/dbCreation']);
     } else if (this.statusData !== null && !this.statusData.isDBUp) {
       this.errorMessageService.add(`Snippets database is not available`);
