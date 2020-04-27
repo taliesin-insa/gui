5f3b306424745c324ad53540d9c51d3ecf1c204a
mdarfay <mdarfay@insa-rennes.fr>
2020-04-27 12:35:05 +0200

    feat(AccountManagement): Handle permissions

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Type } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 import {catchError, map} from 'rxjs/operators';
@@ -9,8 +9,9 @@ import { SessionStorageService } from './session-storage.service';
 import { DbManagementComponent } from '../db-management/db-management.component';
 import { DbCreationComponent } from '../db-creation/db-creation.component';
 import { DbAddExamplesComponent } from '../db-add-examples/db-add-examples.component';
+import {AccountManagementComponent} from '../account-management/account-management.component';
 
-const PRIVILEGED_COMPONENTS = [DbManagementComponent, DbCreationComponent, DbAddExamplesComponent];
+const PRIVILEGED_COMPONENTS = [DbManagementComponent, DbCreationComponent, DbAddExamplesComponent, AccountManagementComponent];
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
