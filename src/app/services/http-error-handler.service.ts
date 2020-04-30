44d9aadd63b7dd70d5fb20c05b9bad45573588f2
mdarfay <mdarfay@insa-rennes.fr>
2020-04-30 17:30:01 +0200

    fix(AccountManagement): Display error correctly in case username or email are already used

--- src/app/services/http-error-handler.service.ts
+++ src/app/services/http-error-handler.service.ts
@@ -57,6 +57,10 @@ export class HttpErrorHandler {
             this.errorMessageService.add(`L'identifiant ou le mot de passe est incorrect.`);
             this.errorMessageService.close(this.errorMessageService.nbErrors - 1, 5000);
           }
+          if (serviceName === 'AccountManagement') {
+            this.errorMessageService.add(`L'identifiant ou l'adresse email est déjà associé à un autre compte.`);
+            this.errorMessageService.close(this.errorMessageService.nbErrors - 1, 10000);
+          }
           break;
         case 404:
           message = `Server or URL not found (` + error.message + ' // ' + backendError + ')';
