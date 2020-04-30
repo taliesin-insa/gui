2b0f1e0619bf29325efe5ce821f7aeb0ea6e1bd7
mdarfay <mdarfay@insa-rennes.fr>
2020-04-30 17:30:01 +0200

    fix(AccountManagement): Display error correctly in case username or email are already used

--- src/app/account-management/account-management.component.ts
+++ src/app/account-management/account-management.component.ts
@@ -64,7 +64,7 @@ export class AccountManagementComponent implements OnInit {
       role: ['', Validators.required]
     });
 
-    this.handleError = httpErrorHandler.createHandleError('Account Management');
+    this.handleError = httpErrorHandler.createHandleError('AccountManagement');
   }
 
   ngOnInit() {
