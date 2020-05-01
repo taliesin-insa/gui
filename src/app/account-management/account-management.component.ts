7d71668e849b3030ee960a430ad5f5648c4dd82a
mdarfay <mdarfay@insa-rennes.fr>
2020-05-01 13:30:28 +0200

    feat(AccountManagement): Add validator on username

--- src/app/account-management/account-management.component.ts
+++ src/app/account-management/account-management.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {Account} from '../model/Account';
-import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../services/auth.service';
 import {SessionStorageService} from '../services/session-storage.service';
 import {HandleError, HttpErrorHandler} from '../services/http-error-handler.service';
 import {catchError} from 'rxjs/operators';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {CustomValidators} from '../custom-validators';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -31,31 +32,34 @@ export class AccountManagementComponent implements OnInit {
               private modalService: NgbModal) {
 
     this.newAccForm = this.fb.group({
-      username: ['', Validators.required],
-      email: ['', Validators.compose([Validators.email, Validators.required])],
-      password: ['', Validators.compose([
-        // 1. Password Field is Required
-        Validators.required,
-        // 2. check whether the entered password has a number
-        CustomValidators.patternValidator(/\d/, { hasNumber: true }),
-        // 3. check whether the entered password has upper case letter
-        CustomValidators.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
-        // 4. check whether the entered password has a lower-case letter
-        CustomValidators.patternValidator(/[a-z]/, { hasSmallCase: true }),
-        // 5. check whether the entered password has a special character
-        CustomValidators.patternValidator(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, { hasSpecialCharacters: true }),
-        // 6. Has a minimum length of 8 characters
-        Validators.minLength(8)])
-      ],
-      confirmPassword: ['', Validators.required],
-      role: ['']
-    },
-    {
-      // check whether password and confirm password match
-      validators: [CustomValidators.passwordMatchValidator]
+        username: ['', Validators.compose([
+          Validators.required,
+          CustomValidators.patternValidator(/[^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {noSpecialCharacter: true})])
+        ],
+        email: ['', Validators.compose([Validators.email, Validators.required])],
+        password: ['', Validators.compose([
+          // 1. Password Field is Required
+          Validators.required,
+          // 2. check whether the entered password has a number
+          CustomValidators.patternValidator(/\d/, {hasNumber: true}),
+          // 3. check whether the entered password has upper case letter
+          CustomValidators.patternValidator(/[A-Z]/, {hasCapitalCase: true}),
+          // 4. check whether the entered password has a lower-case letter
+          CustomValidators.patternValidator(/[a-z]/, {hasSmallCase: true}),
+          // 5. check whether the entered password has a special character
+          CustomValidators.patternValidator(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {hasSpecialCharacters: true}),
+          // 6. Has a minimum length of 8 characters
+          Validators.minLength(8)])
+        ],
+        confirmPassword: ['', Validators.required],
+        role: ['']
+      },
+      {
+        // check whether password and confirm password match
+        validators: [CustomValidators.passwordMatchValidator]
         // CustomValidators.freeUsernameValidator(this.accounts),
         // CustomValidators.freeEmailValidator(this.accounts)]
-    });
+      });
 
     this.changeAccForm = this.fb.group({
       username: ['', Validators.required],
@@ -75,23 +79,23 @@ export class AccountManagementComponent implements OnInit {
     this.newAccForm.reset();
 
     this.auth.accountList(this.session.getToken())
-    .pipe(catchError(this.handleError('listing accounts', null)))
-    .subscribe(data => {
-      if (data !== null) {
-        for (const item of data.body) {
-          this.accounts.push(new Account(item.Username, item.Email, item.Role === 0));
+      .pipe(catchError(this.handleError('listing accounts', null)))
+      .subscribe(data => {
+        if (data !== null) {
+          for (const item of data.body) {
+            this.accounts.push(new Account(item.Username, item.Email, item.Role === 0));
+          }
         }
-      }
-    });
+      });
   }
 
   deleteAccount(account: Account) {
     this.auth.deleteAccount(account.username, this.session.getToken())
-    .pipe(catchError(this.handleError('deleting account', null)))
-    .subscribe(() => {
-      this.selectedAccount = null;
-      this.reloadAccountList();
-    });
+      .pipe(catchError(this.handleError('deleting account', null)))
+      .subscribe(() => {
+        this.selectedAccount = null;
+        this.reloadAccountList();
+      });
   }
 
   createNewAccount(values: any) {
@@ -125,47 +129,4 @@ export class AccountManagementComponent implements OnInit {
         this.reloadAccountList();
       });
   }
-}
-
-export class CustomValidators {
-
-  static patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } => {
-      if (!control.value) {
-        // if control is empty return no error
-        return null;
-      }
-
-      // test the value of the control against the regexp supplied
-      const valid = regex.test(control.value);
-
-      // if true, return no error (no error), else return error passed in the second parameter
-      return valid ? null : error;
-    };
-  }
-
-  static passwordMatchValidator(control: AbstractControl) {
-    const password: string = control.get('password').value; // get password from our password form control
-    const confirmPassword: string = control.get('confirmPassword').value; // get password from our confirmPassword form control
-    // compare is the password math
-    if (password !== confirmPassword) {
-      // if they don't match, set an error in our confirmPassword form control
-      control.get('confirmPassword').setErrors({ noPasswordMatch: true });
-    }
-  }
-
-  // static freeUsernameValidator(accounts: Account[]): ValidatorFn {
-  //   return (control: AbstractControl): ValidationErrors | null => {
-  //     const username = control.get('username').value;
-  //     return { usernameNotFree: (accounts.find(acc => (acc.username === username)) !== undefined) };
-  //   };
-  // }
-  //
-  // static freeEmailValidator(accounts: Account[]): ValidatorFn {
-  //   return (control: AbstractControl): ValidationErrors | null => {
-  //     const email = control.get('email').value;
-  //     return { emailNotFree: (accounts.find(acc => (acc.email === email)) !== undefined) };
-  //   };
-  // }
-
 }
---
+++ src/app/custom-validators.ts
@@ -0,0 +1,44 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export class CustomValidators {
+
+  static patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        // if control is empty return no error
+        return null;
+      }
+
+      // test the value of the control against the regexp supplied
+      const valid = regex.test(control.value);
+
+      // if true, return no error (no error), else return error passed in the second parameter
+      return valid ? null : error;
+    };
+  }
+
+  static passwordMatchValidator(control: AbstractControl) {
+    const password: string = control.get('password').value; // get password from our password form control
+    const confirmPassword: string = control.get('confirmPassword').value; // get password from our confirmPassword form control
+    // compare is the password math
+    if (password !== confirmPassword) {
+      // if they don't match, set an error in our confirmPassword form control
+      control.get('confirmPassword').setErrors({ noPasswordMatch: true });
+    }
+  }
+
+  // static freeUsernameValidator(accounts: Account[]): ValidatorFn {
+  //   return (control: AbstractControl): ValidationErrors | null => {
+  //     const username = control.get('username').value;
+  //     return { usernameNotFree: (accounts.find(acc => (acc.username === username)) !== undefined) };
+  //   };
+  // }
+  //
+  // static freeEmailValidator(accounts: Account[]): ValidatorFn {
+  //   return (control: AbstractControl): ValidationErrors | null => {
+  //     const email = control.get('email').value;
+  //     return { emailNotFree: (accounts.find(acc => (acc.email === email)) !== undefined) };
+  //   };
+  // }
+
+}
