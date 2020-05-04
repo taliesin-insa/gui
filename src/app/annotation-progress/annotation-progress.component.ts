9f23ab364326cf5a799d1452a650d7654dc40d9c
mdarfay <mdarfay@insa-rennes.fr>
2020-05-04 17:01:19 +0200

    fix(AnnotationProgress): Try to use BehaviorSubject

--- src/app/annotation-progress/annotation-progress.component.ts
+++ src/app/annotation-progress/annotation-progress.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {StatusResolverService} from '../services/data-resolver.service';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,14 +11,14 @@ import {StatusResolverService} from '../services/data-resolver.service';
 })
 export class AnnotationProgressComponent implements OnInit {
 
-  annotationRate: number;
-  rejectedNumber: number;
-  totalNbSnippets: number;
+  annotationRate: BehaviorSubject<number>;
+  rejectedNumber: BehaviorSubject<number>;
+  totalNbSnippets: BehaviorSubject<number>;
+
   statusData: any;
 
   constructor(private route: ActivatedRoute,
-              private statusResolverService: StatusResolverService,
-              private changeDetectorRef: ChangeDetectorRef) { }
+              private statusResolverService: StatusResolverService) { }
 
   ngOnInit() {
     // get the data returned by the resolve service
@@ -30,21 +31,20 @@ export class AnnotationProgressComponent implements OnInit {
     this.statusResolverService.getDBStatusRequest().subscribe(data => {
       this.statusData = data;
       this.updateProgress();
-      this.changeDetectorRef.detectChanges(); // kind of a bad practice apparently but didn't find an easier solution to update the view
     });
   }
 
   private updateProgress() {
     // update variables used
     if (this.statusData !== null && this.statusData.isDBUp && this.statusData.total > 0) {
-      this.annotationRate = 100 * this.statusData.annotated / (this.statusData.total - this.statusData.unreadable);
-      this.rejectedNumber = this.statusData.unreadable;
-      this.totalNbSnippets = this.statusData.total;
+      this.annotationRate.next( 100 * this.statusData.annotated / (this.statusData.total - this.statusData.unreadable) );
+      this.rejectedNumber.next( this.statusData.unreadable );
+      this.totalNbSnippets.next( this.statusData.total );
     } else {
       // values by default if there is no database
-      this.annotationRate = 0;
-      this.rejectedNumber = 0;
-      this.totalNbSnippets = 0;
+      this.annotationRate.next(0);
+      this.rejectedNumber.next(0);
+      this.totalNbSnippets.next(0);
     }
   }
 
