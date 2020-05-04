c957da2abed9eb074ac8bcb76bda18128dc7400e
mdarfay <mdarfay@insa-rennes.fr>
2020-05-04 14:50:29 +0200

    feat(AnnotationProgress): Create component to display annotation progress

---
+++ src/app/annotation-progress/annotation-progress.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'annotation-progress',
+  templateUrl: './annotation-progress.component.html',
+  styleUrls: ['./annotation-progress.component.scss']
+})
+export class AnnotationProgressComponent implements OnInit {
+
+  private annotationRate: number;
+  private rejectedNumber: number;
+  private totalNbSnippets: number;
+  private statusData: any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // get the data returned by the resolve service
+    this.statusData = this.route.snapshot.data.statusData;
+    // update variables used
+    if (this.statusData !== null && this.statusData.isDBUp && this.statusData.total > 0) {
+      this.annotationRate = 100 * this.statusData.annotated / (this.statusData.total - this.statusData.unreadable);
+      this.rejectedNumber = this.statusData.unreadable;
+      this.totalNbSnippets = this.statusData.total;
+    } else {
+      // values by default if there is no database
+      this.annotationRate = 0;
+      this.rejectedNumber = 0;
+      this.totalNbSnippets = 0;
+    }
+  }
+
+}
